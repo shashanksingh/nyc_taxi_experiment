25bc86af41c8960ec88493bfc5c5cf7cb08b83d3
Shashank Singh <shashank.personal@gmail.com>
2024-06-25 18:04:14 +0100

    more models added

---
+++ models/mart/manual/dim_payment_type.sql
@@ -0,0 +1,15 @@
+{{
+    config(materialized='table')
+}}
+
+SELECT 1 as id , 'Credit card' as payment_type
+UNION
+SELECT 2 as id , 'Cash' as payment_type
+UNION
+SELECT 3 as id , 'No charge' as payment_type
+UNION
+SELECT 4 as id , 'Dispute' as payment_type
+UNION
+SELECT 5 as id , 'Unknown' as payment_type
+UNION
+SELECT 2 as id , 'Voided trip' as payment_type
--- models/staging/clean_source.sql
+++ models/staging/clean_source.sql
@@ -1,8 +1,24 @@
 {{
-    config(materialized='table')
+    config(
+        materialized='table',
+        post_hook=["CREATE INDEX idx_vendor_id on {{this}} (vendor_id)"]
+    )
 }}
 
 WITH source AS (
     SELECT * FROM {{ref('extract')}}
 )
-SELECT
+SELECT VendorID as vendor_id, 
+    tpep_pickup_datetime as pickup_datetime,
+    tpep_dropoff_datetime as dropoff_datetime,
+    passenger_count,
+    trip_distance,
+    RatecodeID as rate_code_id,
+    store_and_fwd_flag,
+    PULocationID as pickup_location_id,
+    DOLocationID as dropoff_location_id,
+    payment_type as payment_type_id,
+    fare_amount,
+    Airport_fee as airport_fee,
+    *
+    FROM source
